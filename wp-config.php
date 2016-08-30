3df089de2cf53d31cb9371010c2a839d482ac9e9
Ly Vinh Truong <truonglv@evolableasia.vn>
2016-08-30 21:33:02 +0700

    Modify config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'thienphuocloc.com');
+define('DB_NAME', 'u355796579_db');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u355796579_tpl');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'tpl2016');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
